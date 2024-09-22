0fb5d3abc98b30c2a304dfb7da9b782f8635141a
qriechan <qriechan@gmail.com>
2024-09-22 03:42:25 +0100

    added warning

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,6 +10,8 @@ function About() {
         <p>4. Go to the workskins page and copy the CSS</p>
         <p>5. Paste the CSS into the workskins section of the posting page (or wherever else you're calling the CSS from)</p>
         <p>Done!</p>
+        <br />
+        <p>NOTE: All the text functions should work perfectly, but the sticker and picture functions haven't been thoroughly tested yet, so please forgive any bugs and do tell me if you bump into any! More stickers can be added later on request.</p>
     </div>
   )
 }
